c9cbdf7f0d9e2afaba5a95e890d7bf6915492838
PlaySx <gabrierllopes20031978@gmail.com>
2024-03-26 06:45:43 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -43,7 +43,6 @@ public class Order implements Serializable{
 	private Set<OrderItem> items = new HashSet<>();
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	
 	private Payment payment;
 	
 	public Order() {
@@ -102,7 +101,14 @@ public class Order implements Serializable{
 	public Set<OrderItem> getItems(){
 		return items;
 	}
-
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
